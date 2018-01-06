77b7b845f489f4d7ac10fe0c3f6eb3b1de0adc44
Chethan D C <Chethandc2014@gmail.com>
2018-01-06 20:46:47 +0530

    Creating SpringBoot and Angular 4 integration project

---
+++ src/main/java/com/springboot/irctc/SpringBootAgular4Application.java
@@ -0,0 +1,12 @@
+package com.springboot.irctc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAgular4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAgular4Application.class, args);
+	}
+}
